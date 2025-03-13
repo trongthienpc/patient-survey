aac8aef03d8034c4d269c36608fe646e9c0d18d6
ITPC <pcking60@gmail.com>
2025-03-13 16:14:33 +0700

    update prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,11 @@
 import { PrismaClient as PrismaClientDb1 } from "../../prisma/generated/db1";
-import { PrismaClient as PrismaClientDb2 } from "../../prisma/doctors/generated/db2";
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrismaDb1: PrismaClientDb1 | undefined;
   // eslint-disable-next-line no-var
-  var cachedPrismaDb2: PrismaClientDb2 | undefined;
 }
 
 export let prismaDb1: PrismaClientDb1;
-export let prismaDb2: PrismaClientDb2;
 
 if (process.env.NODE_ENV === "production") {
   // Create new PrismaClient instances for production
@@ -16,14 +13,6 @@ if (process.env.NODE_ENV === "production") {
     datasources: {
       db: {
         url: process.env.DATABASE_URL_1, // Connection string for database 1
-      },
-    },
-  });
-
-  prismaDb2 = new PrismaClientDb2({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL_2, // Connection string for database 2
       },
     },
   });
@@ -38,16 +27,6 @@ if (process.env.NODE_ENV === "production") {
       },
     });
   }
-  if (!global.cachedPrismaDb2) {
-    global.cachedPrismaDb2 = new PrismaClientDb2({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL_2,
-        },
-      },
-    });
-  }
 
   prismaDb1 = global.cachedPrismaDb1;
-  prismaDb2 = global.cachedPrismaDb2;
 }
